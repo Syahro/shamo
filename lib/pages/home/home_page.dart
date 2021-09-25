bf411d797c801786ac33957599788ef6f7109cf4
Arief Syahroni <riefsyah31@gmail.com>
2021-09-25 16:22:42 +0700

    add header at homePage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,8 +4,54 @@ import 'package:shamo/theme.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page', style: primaryTextStyle,),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Alex',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: subtitleTextStyle.copyWith(
+                        fontSize: 16, fontWeight: regular),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/image_profile.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
