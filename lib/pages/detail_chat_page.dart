5f5b8e43d50d7f2dc8f66fb870845fd49c783296
Arief Syahroni <riefsyah31@gmail.com>
2021-09-27 15:45:42 +0700

    done add chatPreviewProductPage

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -42,9 +42,121 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(
+          bottom: 20,
+        ),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: primaryColor),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: regular,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/btn_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            productPreview(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    margin: EdgeInsets.only(right: 20),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                            hintText: 'Type Message...',
+                            hintStyle: subtitleTextStyle),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 45,
+                  height: 45,
+                  padding: EdgeInsets.all(13),
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Image.asset(
+                    'assets/btn_send.png',
+                    width: 19,
+                    height: 16,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       extendBody: true,
       backgroundColor: backgroundColor3,
+      bottomNavigationBar: chatInput(),
       appBar: header(),
     );
   }
