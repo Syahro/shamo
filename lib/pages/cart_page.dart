225be5f0bf61cee00261060e40bd3652be86439e
Arief Syahroni <riefsyah31@gmail.com>
2021-10-01 08:51:42 +0700

    done add emptyCart

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class Cartpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Your Cart',
+          style: primaryTextStyle,
+        ),
+      );
+    }
+
+    Widget emptyCart() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon_empty_cart.png',
+              width: 80,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Opss! Your Cart is Empty',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: secondaryTextStyle,
+            ),
+            Container(
+              height: 44,
+              width: 154,
+              margin: EdgeInsets.only(top: 20),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/home', (route) => false);
+                },
+                style: TextButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+        backgroundColor: backgroundColor3, appBar: header(), body: emptyCart());
+  }
+}
