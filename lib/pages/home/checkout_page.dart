dd11f1ccd40094e3302e431e66b52a780b4c9369
Arief Syahroni <riefsyah31@gmail.com>
2021-10-02 16:11:42 +0700

    done add detailAddress on CheckoutPage

--- lib/pages/home/checkout_page.dart
+++ lib/pages/home/checkout_page.dart
@@ -36,12 +36,91 @@ class CheckoutPage extends StatelessWidget {
                   height: 10,
                 ),
                 CheckoutCard(),
-                SizedBox(
-                  height: defaultMargin,
-                ),
               ],
             ),
           ),
+
+          //NOTE: ADDRESS DETAILS
+          Container(
+            margin: EdgeInsets.only(top: defaultMargin),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: backgroundColor4,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Address Details',
+                  style: primaryTextStyle.copyWith(
+                      fontSize: 16, fontWeight: medium),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icon_store_location.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                        Image.asset(
+                          'assets/icon_line.png',
+                          // width: 2,
+                          height: 30,
+                        ),
+                        Image.asset(
+                          'assets/icon_your_address.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Store Location',
+                            style: secondaryTextStyle.copyWith(
+                                fontSize: 12, fontWeight: light),
+                          ),
+                          Text(
+                            'Adidas Core',
+                            style:
+                                primaryTextStyle.copyWith(fontWeight: medium),
+                          ),
+                          SizedBox(
+                            height: defaultMargin,
+                          ),
+                          Text(
+                            'Your Address',
+                            style: secondaryTextStyle.copyWith(
+                                fontSize: 12, fontWeight: light),
+                          ),
+                          Text(
+                            'Marsemoon',
+                            style:
+                                primaryTextStyle.copyWith(fontWeight: medium),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+
+          
+        
         ],
       );
     }
