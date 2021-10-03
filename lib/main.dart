1ef113f509e424c3df235a43fb37669676a7089d
Arief Syahroni <riefsyah31@gmail.com>
2021-10-03 09:11:42 +0700

    done add checkoutSuccessPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/pages/cart_page.dart';
+import 'package:shamo/pages/checkout_success_page.dart';
 import 'package:shamo/pages/detail_chat_page.dart';
 import 'package:shamo/pages/edit_profile_page.dart';
 import 'package:shamo/pages/home/checkout_page.dart';
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         '/product': (context) => ProductPage(),
         '/cart': (context) => Cartpage(),
         '/checkout': (context) => CheckoutPage(),
+        '/checkout-success': (context) => CheckoutSuccessPage(),
       },
     );
   }
---
+++ lib/pages/checkout_success_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class CheckoutSuccessPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Checkout Success'),
+      );
+    }
+
+    Widget content() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon_empty_cart.png',
+              width: 80,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'You made a transaction',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Stay at home while we\nprepare your dream shoes',
+              style: secondaryTextStyle,
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: defaultMargin),
+              height: 44,
+              width: 196,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/home', (route) => false);
+                },
+                child: Text(
+                  'Order Other Shoes',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 44,
+              width: 196,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Color(0XFF39374B),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'View My Order',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                    color: Color(0XFFB7B6BF),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
