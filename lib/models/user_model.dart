49a2848c5db338d7efd8d4f566ba96c095f39b75
Arief Syahroni <riefsyah31@gmail.com>
2021-10-04 08:38:42 +0700

    create userModel and add some library

---
+++ lib/models/user_model.dart
@@ -0,0 +1,40 @@
+class UserModel {
+  int id;
+  String name;
+  String email;
+  String username;
+  String profilePhotoUrl;
+  String token;
+
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.username,
+    this.profilePhotoUrl,
+    this.token,
+  });
+
+  //Constructor apabila dia dari JSON
+  UserModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    username = json['username'];
+    profilePhotoUrl = json['profile_photo_url'];
+    token = json['token'];
+  }
+
+  //Function untuk mengubah UserModel menjadi JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'username': username,
+      'profile_photo_url': profilePhotoUrl,
+      'token': token,
+    };
+  }
+  
+}
